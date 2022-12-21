22158deef430d0279fda193b6e041959accc854b
2s2e <stevendiwenshi1113@gmail.com>
2022-12-20 20:23:06 -0800

    Added some documentation

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) { // listener for tab opens
     if (changeInfo.status == 'loading') { // when the page is loading (you can do info.status === 'complete' but you will see the page for a second or two)
+        
+        //if the url contains reddit.com
         if (tab.url.includes("reddit.com")) {
             chrome.tabs.query({ // change the tab url
                 currentWindow: true,
@@ -11,4 +13,4 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) { // listene
             });
         }
     }
-    })
+})
