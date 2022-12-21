3043286a08bc211cc2afb39dba04094a104473fc
2s2e <stevendiwenshi1113@gmail.com>
2022-12-20 20:19:12 -0800

    Added simple hard-coded redirect, try going onto reddit

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) { // listener for tab opens
+    if (changeInfo.status == 'loading') { // when the page is loading (you can do info.status === 'complete' but you will see the page for a second or two)
+        if (tab.url.includes("reddit.com")) {
+            chrome.tabs.query({ // change the tab url
+                currentWindow: true,
+                active: true
+            }, function (tab) {
+                chrome.tabs.update(tab.id, {
+                    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+                });
+            });
+        }
+    }
+    })
