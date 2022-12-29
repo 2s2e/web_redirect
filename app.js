6ca4960b4049ee9c4ac07478eea8ee015935e443
2s2e <stevendiwenshi1113@gmail.com>
2022-12-28 19:58:58 -0800

    Added working basic persistent storage, may want to consider switching to chrome.storage in the future, but was not able to get sync.set and sync.get to work

--- app.js
+++ app.js
@@ -2,6 +2,31 @@ document.getElementById("save").onclick = function() {
     var toValue = document.getElementById("redirect_to").value;
     var fromValue = document.getElementById("redirect_from").value;
 
-    alert(toValue + " " + fromValue);
+    // const toData = chrome.storage.sync.get(["toKey"]).then((result) => {
+    //     console.log("Value currently is " + result.key);
+    //   });
+
+    // const fromData = chrome.storage.sync.get(["fromKey"]).then((result) => {
+    //     console.log("Value currently is " + result.key);
+    //   });
+
+    // console.log(toData);
+    // console.log(fromData);
+
+    // chrome.storage.sync.set({ toKey : toValue }).then(() => {
+    //     console.log("toValue is set to " + toValue);
+    // });
+
+    // chrome.storage.sync.set({ fromKey : fromValue }).then(() => {
+    //     console.log("fromValue is set to " + fromValue);
+    // });
+    console.log(localStorage["toKey"]);
+    console.log(localStorage["fromKey"]);
+
+    localStorage["toKey"] = toValue;
+    localStorage["fromKey"] = fromValue;
+
+    console.log(localStorage["toKey"]);
+    console.log(localStorage["fromKey"]);
 }
 
