6addcd40ace5463b4c64002d491cb142b5266c46
2s2e <stevendiwenshi1113@gmail.com>
2023-01-04 15:50:17 -0800

    Got basic single user set redirect to work

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.getElementById("save").onclick = function() {
-    var toValue = document.getElementById("redirect_to").value;
-    var fromValue = document.getElementById("redirect_from").value;
+    const toValue = document.getElementById("redirect_to").value;
+    const fromValue = document.getElementById("redirect_from").value;
 
     /*
     // const toData = chrome.storage.sync.get(["toKey"]).then((result) => {
@@ -22,13 +22,37 @@ document.getElementById("save").onclick = function() {
     //     console.log("fromValue is set to " + fromValue);
     // });
     */
-    console.log(localStorage["toKey"]);
-    console.log(localStorage["fromKey"]);
+    // console.log(localStorage["toKey"]);
+    // console.log(localStorage["fromKey"]);
 
-    localStorage["toKey"] = toValue;
-    localStorage["fromKey"] = fromValue;
 
-    console.log(localStorage["toKey"]);
-    console.log(localStorage["fromKey"]);
+    //data is an object: {toKey : "toValue"}
+    chrome.storage.sync.get("toKey", function(data) {
+        //gets the value of the key "toKey" in the object
+        console.log(data.toKey);
+    });
+
+    //data is an object: {fromKey : "fromValue"}
+    chrome.storage.sync.get("fromKey", function(data) {
+        //gets the value of the key "fromKey" in the object
+        console.log(data.fromKey);
+    });
+
+
+    chrome.storage.sync.set({ "toKey" : toValue }, function() {});
+    chrome.storage.sync.set({ "fromKey" : fromValue }, function() {});
+
+
+    //same as above
+    chrome.storage.sync.get("toKey", function(data) {
+        console.log(data.toKey);
+    });
+
+    chrome.storage.sync.get("fromKey", function(data) {
+        console.log(data.fromKey);
+    });
+
+    
+
 }
 
--- background.js
+++ background.js
@@ -1,16 +1,29 @@
 
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) { // listener for tab opens
+chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) { // listener for tab opens
     if (changeInfo.status == 'loading') { // when the page is loading (you can do info.status === 'complete' but you will see the page for a second or two)
         
+        const redirect_to_obj = await chrome.storage.sync.get("toKey");
+        const redirect_from_obj = await chrome.storage.sync.get("fromKey");
+
+        var redirect_to = redirect_to_obj.toKey;
+        var redirect_from = redirect_from_obj.fromKey;
+
+        if(redirect_to.includes("https://")) {
+            redirect_to = redirect_to.replace("https://", "");
+        }
+
+        console.log(redirect_to);
+        console.log(redirect_from);
+
         //if the url contains reddit.com
-        if (tab.url.includes("reddit.com")) {
+        if (tab.url.includes(redirect_from)) {
             chrome.tabs.query({ // change the tab url
                 currentWindow: true,
                 active: true
             }, function (tab) {
                 chrome.tabs.update(tab.id, {
-                    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+                    url: "https://" + redirect_to 
                 });
             });
         }
