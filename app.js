ed9b0a41d1fe1d176f7ae6885514d85fc2106c38
JoshuaKave <jmkav192@gmail.com>
2023-01-09 12:53:27 -0800

    added multiple redirects

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
 window.onload = async function() {
     console.log("start!");
-    
-    //get the values from chrome storage, note the await keyword, this is because the get function is asynchronous
-    const redirect_to_obj = await chrome.storage.sync.get("toKey");
-    const redirect_from_obj = await chrome.storage.sync.get("fromKey");
+    /*//get the values from chrome storage, note the await keyword, this is because the get function is asynchronous
+    const redirect_to_obj = await chrome.storage.sync.get(toValue);
+    const redirect_from_obj = await chrome.storage.sync.get(fromValue);
 
     //for some reason, the key of the the object is toKey, redirect_to and redirect_from store the text values
     var redirect_to = redirect_to_obj.toKey;
-    var redirect_from = redirect_from_obj.fromKey;
+    var redirect_from = redirect_from_obj.fromKey;*/
 
-    //on startup, make it so that the text boxes are filled with the values from chrome storage
-    const toText = document.getElementById("redirect_to");
-    const fromText = document.getElementById("redirect_from");
-
-    toText.value = redirect_to;
-    fromText.value = redirect_from;
 
 }
 
 document.getElementById("save").onclick = function() {
     const toValue = document.getElementById("redirect_to").value;
-    const fromValue = document.getElementById("redirect_from").value;
-
+    const fromKey = document.getElementById("redirect_from").value;
+    chrome.storage.sync.set({ [fromKey] : toValue }, function() {});
     /*
     // const toData = chrome.storage.sync.get(["toKey"]).then((result) => {
     //     console.log("Value currently is " + result.key);
@@ -47,7 +40,7 @@ document.getElementById("save").onclick = function() {
 
 
 
-
+    /*
     //data is an object: {toKey : "toValue"}
     chrome.storage.sync.get("toKey", function(data) {
         //gets the value of the key "toKey" in the object
@@ -58,13 +51,12 @@ document.getElementById("save").onclick = function() {
     chrome.storage.sync.get("fromKey", function(data) {
         //gets the value of the key "fromKey" in the object
         console.log(data.fromKey);
-    });
+    });*/
 
 
-    chrome.storage.sync.set({ "toKey" : toValue }, function() {});
-    chrome.storage.sync.set({ "fromKey" : fromValue }, function() {});
+    
 
-
+    /*
     //same as above
     chrome.storage.sync.get("toKey", function(data) {
         console.log(data.toKey);
@@ -74,7 +66,11 @@ document.getElementById("save").onclick = function() {
         console.log(data.fromKey);
     });
 
-    
-
+    chrome.storage.sync.get(null, function(data) {
+        for (key in data) {
+           // console.log(items.toKey);
+        }
+     });
+*/
 }
 
--- background.js
+++ background.js
@@ -5,31 +5,30 @@ chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) { // l
         
 
         //get the values from chrome storage, note the await keyword, this is because the get function is asynchronous
-        const redirect_to_obj = await chrome.storage.sync.get("toKey");
-        const redirect_from_obj = await chrome.storage.sync.get("fromKey");
+        const all = await chrome.storage.sync.get();
+        for (const [key, val] of Object.entries(all)) {
 
-        //for some reason, the key of the the object is toKey, redirect_to and redirect_from store the text values
-        var redirect_to = redirect_to_obj.toKey;
-        var redirect_from = redirect_from_obj.fromKey;
-
+        const keys = key;
+        const vals = val;
         //make it so that redirect_to accepts both urls with https and without https
-        if(redirect_to.includes("https://")) {
-            redirect_to = redirect_to.replace("https://", "");
-        }
+        if(vals.includes("https://")) {
+            vals = vals.replace("https://", "");
+        }   
 
-        console.log(redirect_to);
-        console.log(redirect_from);
+        //console.log(redirect_to);
+        //console.log(redirect_from);
 
         //if the url contains reddit.com
-        if (tab.url.includes(redirect_from)) {
+        if (tab.url.includes(keys)) {
             chrome.tabs.query({ // change the tab url
                 currentWindow: true,
                 active: true
             }, function (tab) {
                 chrome.tabs.update(tab.id, {
-                    url: "https://" + redirect_to 
+                    url: "https://" + vals 
                 });
             });
         }
+    }
     }
 })
